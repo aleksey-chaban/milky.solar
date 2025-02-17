3e829f4cf8d3a023c8e0c0ecf28c0592da8b2454
Aleksey <achaban@gmail.com>
2025-02-17 10:07:06 -0500

    Changed port from 8080 to 10000

--- app.py
+++ app.py
@@ -300,4 +300,4 @@ def serve_sitemap():
     )
 
 if __name__ == "__main__":
-    waitress.serve(flask_api, host="0.0.0.0", port=8080, threads=8)
+    waitress.serve(flask_api, host="0.0.0.0", port=10000, threads=8)
